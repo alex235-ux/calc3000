afaf4d31aa0813b772e06c41a4bd0a37c287c2b1
alex235-ux <77711230+alex235-ux@users.noreply.github.com>
2021-03-01 11:45:00 +0700

    Add files via upload

---
+++ 3000/app/src/main/java/com/alexux/alexux/a3000/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alexux.alexux.a3000
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
